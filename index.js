66cd6354784e6a4982d328d45848978c02a1d77a
Takuto Wada <takuto.wada@gmail.com>
2019-01-21 14:36:33 +0900

    style: modernize codebase

--- index.js
+++ index.js
@@ -11,100 +11,94 @@
 'use strict';
 /* jshint -W024 */
 
-var estraverse = require('estraverse');
-var espurify = require('espurify');
-var syntax = estraverse.Syntax;
-var hasOwn = Object.prototype.hasOwnProperty;
-var deepEqual = require('deep-equal');
-var notCallExprMessage = 'Argument should be in the form of CallExpression';
-var duplicatedArgMessage = 'Duplicate argument name: ';
-var invalidFormMessage = 'Argument should be in the form of `name` or `[name]`';
+const estraverse = require('estraverse');
+const espurify = require('espurify');
+const syntax = estraverse.Syntax;
+const hasOwn = Object.prototype.hasOwnProperty;
+const deepEqual = require('deep-equal');
+const notCallExprMessage = 'Argument should be in the form of CallExpression';
+const duplicatedArgMessage = 'Duplicate argument name: ';
+const invalidFormMessage = 'Argument should be in the form of `name` or `[name]`';
 
-function CallMatcher (signatureAst, options) {
-    validateApiExpression(signatureAst);
-    options = options || {};
-    this.visitorKeys = options.visitorKeys || estraverse.VisitorKeys;
-    if (options.astWhiteList) {
-        this.purifyAst = espurify.cloneWithWhitelist(options.astWhiteList);
-    } else {
-        this.purifyAst = espurify;
+class CallMatcher {
+    constructor (signatureAst, options) {
+        validateApiExpression(signatureAst);
+        options = options || {};
+        this.visitorKeys = options.visitorKeys || estraverse.VisitorKeys;
+        if (options.astWhiteList) {
+            this.purifyAst = espurify.cloneWithWhitelist(options.astWhiteList);
+        } else {
+            this.purifyAst = espurify;
+        }
+        this.signatureAst = signatureAst;
+        this.signatureCalleeDepth = astDepth(signatureAst.callee, this.visitorKeys);
+        this.numMaxArgs = this.signatureAst.arguments.length;
+        this.numMinArgs = this.signatureAst.arguments.filter(isIdentifier).length;
     }
-    this.signatureAst = signatureAst;
-    this.signatureCalleeDepth = astDepth(signatureAst.callee, this.visitorKeys);
-    this.numMaxArgs = this.signatureAst.arguments.length;
-    this.numMinArgs = this.signatureAst.arguments.filter(identifiers).length;
-}
-
-CallMatcher.prototype.test = function (currentNode) {
-    var calleeMatched = this.isCalleeMatched(currentNode);
-    var numArgs;
-    if (calleeMatched) {
-        numArgs = currentNode.arguments.length;
-        return this.numMinArgs <= numArgs && numArgs <= this.numMaxArgs;
+    test (currentNode) {
+        if (this.isCalleeMatched(currentNode)) {
+            const numArgs = currentNode.arguments.length;
+            return this.numMinArgs <= numArgs && numArgs <= this.numMaxArgs;
+        }
+        return false;
     }
-    return false;
-};
-
-CallMatcher.prototype.matchArgument = function (currentNode, parentNode) {
-    if (isCalleeOfParent(currentNode, parentNode)) {
+    matchArgument (currentNode, parentNode) {
+        if (isCalleeOfParent(currentNode, parentNode)) {
+            return null;
+        }
+        if (this.test(parentNode)) {
+            const indexOfCurrentArg = parentNode.arguments.indexOf(currentNode);
+            let numOptional = parentNode.arguments.length - this.numMinArgs;
+            const matchedSignatures = this.argumentSignatures().reduce((accum, argSig) => {
+                if (argSig.kind === 'mandatory') {
+                    accum.push(argSig);
+                }
+                if (argSig.kind === 'optional' && 0 < numOptional) {
+                    numOptional -= 1;
+                    accum.push(argSig);
+                }
+                return accum;
+            }, []);
+            return matchedSignatures[indexOfCurrentArg];
+        }
         return null;
     }
-    if (this.test(parentNode)) {
-        var indexOfCurrentArg = parentNode.arguments.indexOf(currentNode);
-        var numOptional = parentNode.arguments.length - this.numMinArgs;
-        var matchedSignatures = this.argumentSignatures().reduce(function (accum, argSig) {
-            if (argSig.kind === 'mandatory') {
-                accum.push(argSig);
-            }
-            if (argSig.kind === 'optional' && 0 < numOptional) {
-                numOptional -= 1;
-                accum.push(argSig);
-            }
-            return accum;
-        }, []);
-        return matchedSignatures[indexOfCurrentArg];
+    calleeAst () {
+        return this.purifyAst(this.signatureAst.callee);
     }
-    return null;
-};
-
-CallMatcher.prototype.calleeAst = function () {
-    return this.purifyAst(this.signatureAst.callee);
-};
-
-CallMatcher.prototype.argumentSignatures = function () {
-    return this.signatureAst.arguments.map(toArgumentSignature);
-};
-
-CallMatcher.prototype.isCalleeMatched = function (node) {
-    if (!isCallExpression(node)) {
-        return false;
+    argumentSignatures () {
+        return this.signatureAst.arguments.map(toArgumentSignature);
     }
-    if (!this.isSameDepthAsSignatureCallee(node.callee)) {
-        return false;
-    }
-    return deepEqual(this.purifyAst(this.signatureAst.callee), this.purifyAst(node.callee));
-};
-
-CallMatcher.prototype.isSameDepthAsSignatureCallee = function (ast) {
-    var depth = this.signatureCalleeDepth;
-    var currentDepth = 0;
-    estraverse.traverse(ast, {
-        keys: this.visitorKeys,
-        enter: function (currentNode, parentNode) {
-            var path = this.path();
-            var pathDepth = path ? path.length : 0;
-            if (currentDepth < pathDepth) {
-                currentDepth = pathDepth;
-            }
-            if (depth < currentDepth) {
-                this['break']();
-            }
+    isCalleeMatched (node) {
+        if (!isCallExpression(node)) {
+            return false;
         }
-    });
-    return (depth === currentDepth);
-};
+        if (!this.isSameDepthAsSignatureCallee(node.callee)) {
+            return false;
+        }
+        return deepEqual(this.purifyAst(this.signatureAst.callee), this.purifyAst(node.callee));
+    }
+    isSameDepthAsSignatureCallee (ast) {
+        const depth = this.signatureCalleeDepth;
+        let currentDepth = 0;
+        estraverse.traverse(ast, {
+            keys: this.visitorKeys,
+            enter: function (currentNode, parentNode) {
+                const path = this.path();
+                const pathDepth = path ? path.length : 0;
+                if (currentDepth < pathDepth) {
+                    currentDepth = pathDepth;
+                }
+                if (depth < currentDepth) {
+                    this['break']();
+                }
+            }
+        });
+        return (depth === currentDepth);
+    }
+}
 
-function toArgumentSignature (argSignatureNode, idx) {
+const toArgumentSignature = (argSignatureNode, idx) => {
     switch(argSignatureNode.type) {
     case syntax.Identifier:
         return {
@@ -121,57 +115,49 @@ function toArgumentSignature (argSignatureNode, idx) {
     default:
         return null;
     }
-}
+};
 
-function astDepth (ast, visitorKeys) {
-    var maxDepth = 0;
+const astDepth = (ast, visitorKeys) => {
+    let maxDepth = 0;
     estraverse.traverse(ast, {
         keys: visitorKeys,
         enter: function (currentNode, parentNode) {
-            var path = this.path();
-            var pathDepth = path ? path.length : 0;
+            const path = this.path();
+            const pathDepth = path ? path.length : 0;
             if (maxDepth < pathDepth) {
                 maxDepth = pathDepth;
             }
         }
     });
     return maxDepth;
-}
+};
 
-function isCallExpression (node) {
-    return node && node.type === syntax.CallExpression;
-}
+const isCallExpression = (node) => node && node.type === syntax.CallExpression;
+const isIdentifier = (node) => node.type === syntax.Identifier;
+const isCalleeOfParent = (currentNode, parentNode) => parentNode &&
+          currentNode &&
+          parentNode.type === syntax.CallExpression &&
+          parentNode.callee === currentNode;
 
-function isCalleeOfParent(currentNode, parentNode) {
-    return parentNode && currentNode &&
-        parentNode.type === syntax.CallExpression &&
-        parentNode.callee === currentNode;
-}
-
-function identifiers (node) {
-    return node.type === syntax.Identifier;
-}
-
-function validateApiExpression (callExpression) {
+const validateApiExpression = (callExpression) => {
     if (!callExpression || !callExpression.type) {
         throw new Error(notCallExprMessage);
     }
     if (callExpression.type !== syntax.CallExpression) {
         throw new Error(notCallExprMessage);
     }
-    var names = {};
-    callExpression.arguments.forEach(function (arg) {
-        var name = validateArg(arg);
+    const names = {};
+    callExpression.arguments.forEach((arg) => {
+        const name = validateArg(arg);
         if (hasOwn.call(names, name)) {
             throw new Error(duplicatedArgMessage + name);
         } else {
             names[name] = name;
         }
     });
-}
+};
 
-function validateArg (arg) {
-    var inner;
+const validateArg = (arg) => {
     switch(arg.type) {
     case syntax.Identifier:
         return arg.name;
@@ -179,7 +165,7 @@ function validateArg (arg) {
         if (arg.elements.length !== 1) {
             throw new Error(invalidFormMessage);
         }
-        inner = arg.elements[0];
+        const inner = arg.elements[0];
         if (inner.type !== syntax.Identifier) {
             throw new Error(invalidFormMessage);
         }
@@ -187,6 +173,6 @@ function validateArg (arg) {
     default:
         throw new Error(invalidFormMessage);
     }
-}
+};
 
 module.exports = CallMatcher;
